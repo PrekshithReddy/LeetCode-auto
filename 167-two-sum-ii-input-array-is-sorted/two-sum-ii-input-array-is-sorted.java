1678254481105acc6973cf4e42040d2eb4147551
Prekshith Reddy <prekshithreddy2728@gmail.com>
2026-03-23 18:40:49 +0530

    Time: 2 ms (96.33%) | Memory: 48.3 MB (74.06%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -6,8 +6,7 @@ int right=n-1;
 while(left<right)
 {
     int sum=numbers[left]+numbers[right];
-    if(sum==target)
-    {
+    if(sum==target){
         return new int[]{left+1,right+1};
     }
     else if(sum<target)
